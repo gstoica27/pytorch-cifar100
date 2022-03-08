33d70278e7f992e6423e475fb5c862ac29dc28a2
taylor1355 <hearntaylor@gmail.com>
2022-03-07 20:29:38 -0500

    Add functionality to generate experiment runner script

--- scripts/generate_experiment_runner.py
+++ scripts/generate_experiment_runner.py
@@ -9,78 +9,39 @@ Use this file to bulk run experiments!
 4. Run "python scripts/generate_experiment_runner.py"
 5. This should generate a scripts/run_experiments.sh, which you can execute for your batch run.
 """
+# Logistical Options
+do_resume = False
+require_a40 = False
+num_repetitions = 1
+
 # Specify configs
-approach_name = 'self_attention'
+approach_names = ['self_attention']
 filter_sizes = [1]
 strides = [1]
 stackings = [1]
 injection_points = [
-    [2],
     [3],
-    [4],
-    [5],
-    [2, 3],
-    [2, 4],
-    [2, 5],
-    [3, 4],
-    [3, 5],
-    [4, 5],
-    [2, 3, 4],
-    [2, 3, 5],
-    [3, 4, 5],
-    [2, 3, 4, 5]
 ]
-positional_encodings = [10]
-residuals = ['False', 'True']
+positional_encodings = [0]
+residuals = ['True']
+
 # Aggregate specifications
 experiment_list = []
-for stacking in stackings:
-    for injection_point in injection_points:
-        for pos_enc in positional_encodings:
-            for residual in residuals:
-                for filter_size in filter_sizes:
-                    for stride in strides:
-                        experiment_list.append(
-                            ["resnet", approach_name, filter_size, stride, injection_point, stacking, pos_enc, residual]
-                        )
-
-# experiment_list = [
-#     # backbone, approach, filter size, stride, location, stacking, position encoding, use_residual_connection
-#     ["resnet", approach_name, 3, 1, [5], 1, 10, "False"],
-#     ["resnet", approach_name, 3, 1, [4], 1, 10, "False"],
-#     ["resnet", approach_name, 3, 1, [3], 1, 10, "False"],
-#     ["resnet", approach_name, 3, 1, [2], 1, 10, "False"],
-#     ["resnet", approach_name, 3, 1, [2, 3], 1, 10, "False"],
-#     ["resnet", approach_name, 3, 1, [2, 4], 1, 10, "False"],
-#     ["resnet", approach_name, 3, 1, [2, 5], 1, 10, "False"],
-#     ["resnet", approach_name, 3, 1, [3, 4], 1, 10, "False"],
-#     ["resnet", approach_name, 3, 1, [3, 5], 1, 10, "False"],
-#     ["resnet", approach_name, 3, 1, [4, 5], 1, 10, "False"],
-#     ["resnet", approach_name, 3, 1, [2, 3, 4], 1, 10, "False"],
-#     ["resnet", approach_name, 3, 1, [2, 3, 5], 1, 10, "False"],
-#     ["resnet", approach_name, 3, 1, [3, 4, 5], 1, 10, "False"],
-#     ["resnet", approach_name, 3, 1, [2, 3, 4, 5], 1, 10, "False"],
-# ]
-
-# experiment_list = [
-#     # backbone, approach, filter size, stride, location, stacking, position encoding, use_residual_connection
-#     ["resnet", "3", 3, 1, [3], 1, 0, "False"],
-#     ["resnet", "3", 3, 1, [2,3], 1, 0, "False"],
-#     ["resnet", "3", 3, 1, [2,4], 1, 0, "False"],
-#     ["resnet", "3", 3, 1, [2,5], 1, 0, "False"],
-#     ["resnet", "3", 3, 1, [3,4], 1, 0, "False"],
-#     ["resnet", "3", 3, 1, [3,5], 1, 0, "False"],
-#     ["resnet", "3", 3, 1, [4,5], 1, 0, "False"],
-#     ["resnet", "3", 3, 1, [2,3,4], 1, 0, "False"],
-#     ["resnet", "3", 3, 1, [2,3,5], 1, 0, "False"],
-#     ["resnet", "3", 3, 1, [3,4,5], 1, 0, "False"],
-#     ["resnet", "3", 3, 1, [2,3,4,5], 1, 0, "False"],
-# ]
-
-
-
+for repetition in range(num_repetitions):
+    for approach_name in approach_names:
+        for stacking in stackings:
+            for injection_point in injection_points:
+                for pos_enc in positional_encodings:
+                    for residual in residuals:
+                        for filter_size in filter_sizes:
+                            for stride in strides:
+                                experiment_list.append(
+                                    ["resnet", approach_name, filter_size, stride, injection_point, stacking, pos_enc, residual, repetition]
+                                )
 
 def generate_command(experiment_config, env_name):
+    resume_arg = " -resume" if do_resume else ""
+    a40_constraint_arg = " --constraint=a40" if require_a40 else ""
 
     injection_info = [
         [i, experiment_config[5], experiment_config[2]] for i in experiment_config[4]
@@ -89,10 +50,11 @@ def generate_command(experiment_config, env_name):
     residual_connection_arg = " --use_residual_connection" if experiment_config[7] == 'True' else ""
 
     out = (
-        f"'source /srv/share/gstoica3/miniconda3/etc/profile.d/conda.sh && conda activate {env_name} && "
-        "srun -p overcap -A overcap -t 48:00:00"
-        + ' --gres gpu:1 -c 6 python train.py -net "resnet18" '
+        f"'source /srv/share4/thearn6/miniconda3/etc/profile.d/conda.sh && conda activate {env_name} && "
+        f"srun -p overcap -A overcap{a40_constraint_arg} -t 48:00:00"
+        + f' --gres gpu:1 -c 6 python train.py{resume_arg} -net "resnet18" '
         + f' --approach_name "{experiment_config[1]}"'
+        + f' --suffix "{experiment_config[8]}"'
         + f" --pos_emb_dim {experiment_config[6]}"
         + f' --injection_info "{injection_info}"'
         + f" --stride {experiment_config[3]}"
@@ -109,7 +71,7 @@ def make_executable(path):
     os.chmod(path, mode)
 
 
-def generate_bash_executable(env_name="p3", offset=0):
+def generate_bash_executable(env_name="csam", offset=0):
 
     # Set offset value incase tmux sessions have a chance of having duplicate names
     executable = """#!/bin/bash"""
@@ -137,4 +99,4 @@ def generate_bash_executable(env_name="p3", offset=0):
 
 
 if __name__ == "__main__":
-    generate_bash_executable(env_name="cifar", offset=300)
+    generate_bash_executable(env_name="csam", offset=300)
